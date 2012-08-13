15a7cc9c66d4773170b1ed929d3b993a29694027
TerraNetworkOrg <admin@terra-network.org>
2012-08-13 11:52:20 +0300

    Update src/net/aufdemrand/sentry/SentryInstance.java

--- src/net/aufdemrand/sentry/SentryInstance.java
+++ src/net/aufdemrand/sentry/SentryInstance.java
@@ -285,6 +285,27 @@ public class SentryInstance {
 
 				}
 
+				
+				//*** Added by: TerraNetworkOrg
+				if(plugin.isTownyActive == true) {
+					Player player = (Player) aTarget;
+					Resident resident;
+					try {
+						resident = TownyUniverse.getDataSource().getResident(player.getName());
+						String town;
+						if(resident.hasTown()) {
+							town = resident.getTown().getName();
+							if (this.containsTarget("TOWN:" + town)) {
+								isATarget = true;
+							}	
+						}
+					} catch (NotRegisteredException e) {
+						// TODO Auto-generated catch block
+						//e.printStackTrace();
+					}
+				}				
+				//*** Added by: TerraNetworkOrg
+
 			}
 
 
