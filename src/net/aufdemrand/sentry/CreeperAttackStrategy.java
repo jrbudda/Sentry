4a125f0b2131ff759f146097af4fcb282c1fd20b
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2013-09-20 18:00:12 -0700

    Update to 1.6.4

--- src/net/aufdemrand/sentry/CreeperAttackStrategy.java
+++ src/net/aufdemrand/sentry/CreeperAttackStrategy.java
@@ -1,12 +1,12 @@
 package net.aufdemrand.sentry;
 
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.craftbukkit.v1_6_R2.entity.CraftCreeper;
+import org.bukkit.craftbukkit.v1_6_R3.entity.CraftCreeper;
 
 public class CreeperAttackStrategy implements net.citizensnpcs.api.ai.AttackStrategy{
 
 	@Override
-	public boolean handle(LivingEntity arg0, LivingEntity arg1) {	
+	public boolean handle(LivingEntity arg0, LivingEntity arg1) {
 		((CraftCreeper)arg0).getHandle().a(1);
 		return true;
 	}
